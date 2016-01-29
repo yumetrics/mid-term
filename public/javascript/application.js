40570717c7f9323b591aa14d2d2eee5a4b1dc5fc
sayyl <sayyl@users.noreply.github.com>
2016-01-29 21:38:39 +0000

    added in loop for printing images as li objects

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var $element = $("#coffee_icon");
+  
+// $element.velocity({
+//     width: "500px"
+// }, {
+//     /* Velocity's default options */
+//     duration: 400,
+//     easing: "swing",
+//     queue: "",
+//     begin: undefined,
+//     progress: undefined,
+//     complete: undefined,
+//     display: undefined,
+//     visibility: undefined,
+//     loop: false,
+//     delay: false,
+//     mobileHA: true
+// });
+
 });
+
+
